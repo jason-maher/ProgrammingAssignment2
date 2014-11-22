1da841ea2685617ae87343d0aa59e303011f292b
Jason Maher <jason@jasonmaher.me>
2014-11-22 17:41:11 +0900

    passes all tests, needs comments

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,28 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+    inverse <- NULL
 
+    set <- function(data) {
+        x <<- data
+        inverse <<- NULL
+    }
+    get <- function() { x }
+    setInverse <- function(value) { inverse <<- value }
+    getInverse <- function() { inverse }
+
+    list(set = set,
+         get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    if (is.null(x$getInverse())) {
+        x$setInverse(solve(x$get(), ...))
+    }
+    x$getInverse()
 }
